9fe012996e5ade7a8a8997cdd5e2f9dad630836f
gabriel <gabrielrgrocha14@gmail.com>
2019-11-25 02:11:01 -0300

    Adicionei as controllers

---
+++ PLOSAT/Controller/StoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StoryViewController.swift
+//  PLOSAT
+//
+//  Created by Gabriel Gazal on 25/11/19.
+//  Copyright © 2019 OGrupoMaisBonitoDaCidade. All rights reserved.
+//
+
+import UIKit
+
+class StoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
