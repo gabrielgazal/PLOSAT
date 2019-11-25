d9049619297f48414798903f0be94c3ad542da54
gabriel <gabrielrgrocha14@gmail.com>
2019-11-25 00:04:57 -0300

    Tela inicial

---
+++ PLOSAT/Extensions/DesignableView.swift
@@ -0,0 +1,26 @@
+//
+//  DesignableView.swift
+//  Brasunia
+//
+//  Created by Gabriel Gazal on 28/08/19.
+//  Copyright © 2019 Brasunia. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DesignableView: UIView {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    func updateLayerProperties() {
+           self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+           self.layer.shadowOffset = CGSize(width: 0, height: 3)
+           self.layer.shadowOpacity = 3.0
+           self.layer.shadowRadius = 10.0
+           self.layer.masksToBounds = false
+       }
+}
