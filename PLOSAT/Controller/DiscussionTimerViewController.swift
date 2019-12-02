50e9945686386c51f7930054344ab499e4dc6db8
gabriel <gabrielrgrocha14@gmail.com>
2019-12-02 13:16:10 -0300

    aaa

--- PLOSAT/Controller/DiscussionTimerViewController.swift
+++ PLOSAT/Controller/DiscussionTimerViewController.swift
@@ -10,21 +10,39 @@ import UIKit
 
 class DiscussionTimerViewController: UIViewController {
 
+    @IBOutlet weak var minutosLabel: UILabel!
+    @IBOutlet weak var segundosLabel: UILabel!
+    @IBAction func StartTimer(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+
+    }
+    var timer: Timer!
+    var count = 5{
+        didSet {
+            let minutos = count/60
+            let segundos = count % 60
+            
+            minutosLabel.text = String(format: "%02d",minutos)
+            segundosLabel.text = String(format: "%02d",segundos)
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func update() {
+        if(count > 0) {
+            count -= 1
+        }else {
+            timer.invalidate()
+            if Model.instance.viuModal == false{
+                self.performSegue(withIdentifier: "modalDiscussion", sender: nil)
+            }
+        }
     }
-    */
 
 }
