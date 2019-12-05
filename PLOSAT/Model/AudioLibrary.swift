c96311b09d4e7e5802e68ebc0555eb74be926c61
gabriel <gabrielrgrocha14@gmail.com>
2019-12-05 16:56:12 -0300

    audios funcionando

--- PLOSAT/Model/AudioLibrary.swift
+++ PLOSAT/Model/AudioLibrary.swift
@@ -11,5 +11,9 @@ enum SongLibrary : String, CaseIterable {
 
 
 enum SoundEffectLibrary : String, CaseIterable {
-    case button = "Clique-do-Botão-_mais-curto_.mp3"
+    case button = "click.mp3"
+    case timer = "Contagem-Regressiva-2.mp3"
+    case jail = "Large-steel-door-closing.mp3"
+    case inocente = "Acusado-Inocente-_Dramático_.mp3"
+    case culpado = "Acusado-Culpado-_menos-épico_.mp3"
 }
