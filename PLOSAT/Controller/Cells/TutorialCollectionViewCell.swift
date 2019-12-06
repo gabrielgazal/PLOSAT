a963ea4fcfd84cd54d01365b4791a983544c6d79
gabriel <gabrielrgrocha14@gmail.com>
2019-12-06 16:05:53 -0300

    tela de tutorial

---
+++ PLOSAT/Controller/Cells/TutorialCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TutorialCollectionViewCell.swift
+//  PLOSAT
+//
+//  Created by Gabriel Gazal on 06/12/19.
+//  Copyright © 2019 OGrupoMaisBonitoDaCidade. All rights reserved.
+//
+
+import UIKit
+
+class TutorialCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imagem: UIImageView!
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var descricao: UILabel!
+    
+}
